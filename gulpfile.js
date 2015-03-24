a3fc18464b3b1cee585e9536a4b2978246e22676
Ken Smith <ken.smith@2scientists.net>
2015-03-24 12:03:23 -0400

    Attempting to add bower deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 var gulp = require('gulp');
 var deploy = require('gulp-gh-pages');
+var wiredep = require('wiredep').stream;
 
 /**
  * Push build to gh-pages
  */
-gulp.task('deploy', function() {
+gulp.task('deploy', ['copy', 'bower'], function() {
   return gulp.src("./dist/**/*")
     .pipe(deploy())
+});
+
+gulp.task('copy', function() {
+  gulp.src('./bower_components')
+    .pipe(gulp.dest('./dist'));
+});
+
+/**
+ * Use wiredep to weave in bower dependencies 
+ */
+gulp.task('bower', function() {
+  gulp.src('./index.html')
+    .pipe(wiredep())
+    .pipe(gulp.dest('./dist'));
 });
