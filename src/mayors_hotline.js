85aa308b5be5647ea586c816fa00048a69850ad3
Ken Smith <ken.smith@2scientists.net>
2015-03-26 08:28:47 -0400

    Switching to 30day view

--- src/mayors_hotline.js
+++ src/mayors_hotline.js
@@ -1,4 +1,6 @@
-var dateChart = dc.barChart("#date-chart");
+'use strict';
+
+var dateChart = dc.lineChart("#date-chart");
 var hourChart = dc.barChart("#hour-chart");
 var dayChart = dc.rowChart("#day-chart");
 var sourceChart = dc.rowChart("#source-chart");
@@ -48,13 +50,16 @@ var updateMap = function(locs) {
   });
 };
 
+var today = new Date();
+var thirty_days_ago = new Date(today.getTime() - 30*24*60*60*1000);
+var tda_date = thirty_days_ago.toISOString().substring(0,10);
 
 // Note that we're currently limiting the amount of data retrieved from the Socrata API
 // Their API's maximum limit is 50,000 records. There are about 500,000 in the dataset.
 // May need to switch to a local CSV  to access the full dataset.
 
 // Add $order query param to be able to see most recent data.
-d3.json("https://data.cityofboston.gov/resource/awu8-dc52?$limit=1000", function(err, data) {
+d3.json("https://data.cityofboston.gov/resource/awu8-dc52?$limit=50000&$where=open_dt>'"+tda_date+"'", function(err, data) {
   var dateFormat = d3.time.format("%Y-%m-%dT%H:%M:%S");
   data.forEach(function(d) {
     d.date_opened = dateFormat.parse(d.open_dt);
@@ -72,7 +77,7 @@ d3.json("https://data.cityofboston.gov/resource/awu8-dc52?$limit=1000", function
   var all = index.groupAll();
 
   var sources = index.dimension( function(d) { return d.source; });
-  var open_dates = index.dimension( function(d) { return d3.time.day(d.date_opened); } );
+  var open_dates = index.dimension( function(d) { return d3.time.hour(d.date_opened); } );
   var open_hours = index.dimension( function(d) { return d.date_opened.getHours()+1; } );
   var open_days = index.dimension( function(d) { 
     var day = d.date_opened.getDay();
@@ -84,29 +89,38 @@ d3.json("https://data.cityofboston.gov/resource/awu8-dc52?$limit=1000", function
   var reasons = index.dimension( function(d) { return d.reason } ); 
   locations = index.dimension( function(d) { return d.geocoded_location; });
 
+  dc.dataCount('.data-count')
+    .dimension(index)
+    .group(all);
+    // .html({
+    //     some:'<strong>%filter-count</strong> selected out of <strong>%total-count</strong> records' +
+    //         ' | <a href=\'javascript:dc.filterAll(); dc.renderAll();\'\'>Reset All</a>',
+    //     all:'All records selected. Please click on the graph to apply filters.'
+    // });
+
   dateChart
     .width($('#date-chart').innerWidth()-30)
     .height(150)
-    .margins({top: 10, left:20, right: 10, bottom:20})
-    .x(d3.time.scale().domain([new Date(2011, 6, 1), new Date(2015, 3, 4)]))
+    .margins({top: 10, left:30, right: 10, bottom:20})
+    .x(d3.time.scale().domain([thirty_days_ago, today]))
     .colors(singleColor)
     .dimension(open_dates)
     .group(open_dates.group())
     .elasticY(true)
     .yAxis().ticks(6);
-  dateChart.on("filtered", onFiltered);
+  //dateChart.on("filtered", onFiltered);
 
   hourChart
     .width($('#hour-chart').innerWidth()-30)
     .height(227)
-    .margins({top: 10, left:30, right: 10, bottom:20})
+    .margins({top: 10, left:35, right: 10, bottom:20})
     .x(d3.scale.linear().domain([1,24]))
     .colors(singleColor)
     .dimension(open_hours)
     .group(open_hours.group())
     .gap(1)
     .elasticY(true);
-  hourChart.on("filtered", onFiltered);
+  //hourChart.on("filtered", onFiltered);
 
   dayChart
     .width($('#day-chart').innerWidth()-30)
@@ -120,7 +134,7 @@ d3.json("https://data.cityofboston.gov/resource/awu8-dc52?$limit=1000", function
     .elasticX(true)
     .gap(1)
     .xAxis().ticks(0);
-  dayChart.on("filtered", onFiltered);
+  //dayChart.on("filtered", onFiltered);
 
   statusChart
     .width($('#status-chart').innerWidth()-30)
@@ -131,7 +145,7 @@ d3.json("https://data.cityofboston.gov/resource/awu8-dc52?$limit=1000", function
     .gap(1)
     .dimension(status)
     .xAxis().ticks(0);
-  statusChart.on("filtered", onFiltered);
+  //statusChart.on("filtered", onFiltered);
 
   sourceChart
     .width($('#source-chart').innerWidth()-30)
@@ -144,7 +158,7 @@ d3.json("https://data.cityofboston.gov/resource/awu8-dc52?$limit=1000", function
     .gap(1)
     .ordering(function(i){return -i.value;})
     .xAxis().ticks(0);
-  sourceChart.on("filtered", onFiltered);
+  //sourceChart.on("filtered", onFiltered);
 
   neighborhoodChart
     .width($('#neighborhood-chart').innerWidth()-30)
@@ -156,11 +170,11 @@ d3.json("https://data.cityofboston.gov/resource/awu8-dc52?$limit=1000", function
     .elasticX(true)
     .gap(1)
     .ordering(function(i){return -i.value;});
-  neighborhoodChart.on("filtered", onFiltered);
+  //neighborhoodChart.on("filtered", onFiltered);
 
   reasonChart
     .width($('#reason-chart').innerWidth()-30)
-    .height(600)
+    .height(1000)
     .margins({top: 10, left:5, right: 10, bottom:20})
     .colors(singleColor)
     .group(reasons.group())
@@ -168,7 +182,7 @@ d3.json("https://data.cityofboston.gov/resource/awu8-dc52?$limit=1000", function
     .elasticX(true)
     .gap(1)
     .ordering(function(i){return -i.value;});
-  reasonChart.on("filtered", onFiltered);
+  //reasonChart.on("filtered", onFiltered);
 
   dc.renderAll();
 
