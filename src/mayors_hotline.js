54138cddf3a02d6177d7d94b704e8ab6f7c51ca0
Ken Smith <ken.smith@2scientists.net>
2015-03-29 08:56:36 -0400

    fixes #3 recalcs width on window resize

--- src/mayors_hotline.js
+++ src/mayors_hotline.js
@@ -11,6 +11,17 @@ var openDaysChart = dc.rowChart("#opendays-chart");
 var dataTable = dc.dataTable("#data-table")
 var dataCount = dc.dataCount('.data-count');
 
+var allCharts = [
+  {chart: dateChart, id: "#date-chart"},
+  {chart: hourChart, id: "#hour-chart"},
+  {chart: dayChart,  id: "#day-chart"},
+  {chart: sourceChart, id: "#source-chart"},
+  {chart: statusChart, id: "#status-chart"},
+  {chart: neighborhoodChart, id: "#neighborhood-chart"},
+  {chart: reasonChart, id: "#reason-chart"},
+  {chart: openDaysChart, id: "#opendays-chart"}
+];
+
 var singleColor = ["#1a8bba"];
 
 
@@ -227,3 +238,15 @@ d3.csv(boston_data_url, function(err, data) {
   updateMap(locations.top(Infinity));
 
 });
+
+window.onresize = function(event) {
+  allCharts.forEach(function(chart) {
+    // Disable redraw animation first to prevent jitter while resizing window
+    chart.chart.transitionDuration(0).width($(chart.id).innerWidth()-30);
+  });
+  dc.renderAll();
+  // Set transition back to default:
+  allCharts.forEach(function(chart) {
+    chart.chart.transitionDuration(750);
+  });
+};
